8e19c6c9284b5689dc144caed4cc456b21e2de60
Mads Knudsen <mads@rknudsen.dk>
2025-03-04 12:09:45 +0100

    feat(Database and docker-compose): First version with MariaDB and Docker Compose

---
+++ db/migrations/V1__createhistorytable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE history (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstvalue DECIMAL(10,2) NOT NULL,
+    secondvalue DECIMAL(10,2) NOT NULL,
+    result DECIMAL(10,2) NOT NULL
+);
